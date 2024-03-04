5e157215cd97fcccc2b8d5517cea1e7d0b763569
Angel Pessolano <angel.pessolano@gmail.com>
2024-03-04 16:23:12 -0400

    Cambio Cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import productsRoutes from "./routes/products.routes";
 
+var cors = require('cors')
+
 const app = express();
+
+
+
+  
 //settings
 //app.set('port',config.port)
 
 app.use(express.json());
+app.use(cors());
 //app.use(express.urlencoded({ extended: false }));
 app.use(productsRoutes);
 
